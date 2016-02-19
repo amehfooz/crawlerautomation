c0e2f7cf5d29a9df5f85603518a9c14f710c47cd
amehfooz <ahmed.mehfooz@gmail.com>
2016-02-19 12:17:42 -0800

    Completed Database Functions

--- DataBase.py
+++ DataBase.py
@@ -11,41 +11,73 @@ import mysql.connector
 from bigquery import get_client
 
 class DataBase(object):
-    def __init__(self):
+    def __init__(self, scantable):
         try:
             self.conn = mysql.connector.connect(host=HOST, database=DB, user= USER, password = PWD)
         except Exception as e:
             print 'DB Connection Error'
             return
 
-        query = """CREATE TABLE IF NOT EXISTS `scanTable` (
+        self.scantable = scantable
+        query = """CREATE TABLE IF NOT EXISTS `%s` (
                 `Name` Varchar(20) NOT NULL,
-                `URL` Varchar(50) NOT NULL,
-                `LastScanned` Date NOT NULL,
-                `ScanStatus` Boolean,
+                `URL` Varchar(120) NOT NULL,
+                `LastScanned` Date DEFAULT NULL,
+                `ScanStatus` Boolean Default FALSE,
                 PRIMARY KEY (`URL`))
-            """
+            """ % self.scantable
         cursor = self.conn.cursor()
 
         cursor.execute(query)
     
     def populate(self, table):
         client = get_client(project_id, json_key_file=json_key, readonly=False)
-        qry = "SELECT Name, Url  FROM [%s] LIMIT 10" % table
+        qry = "SELECT Name, Url  FROM [%s] LIMIT 5" % table
 
         try:
             job_id, results = client.query(qry, timeout=3000)
 
             results = client.get_query_rows(job_id)
 
-            print results[0]
+            for row in results:
+                query = """
+                INSERT INTO %s (Name, URL) VALUES ('%s', '%s')
+                ON DUPLICATE KEY UPDATE URL = '%s'
+                """ % (self.scantable, row['Name'].replace('\'', ''), row['Url'], row['Url'])
+                
+                cursor = self.conn.cursor()
+                cursor.execute(query)
+
+            self.conn.commit()
         except Exception as e:
             print e
             print 'timeout'
 
-        
-    def quit():
-        self.conn.close()
+    def toCrawl(self):
+        query = """SELECT URL FROM %s WHERE ScanStatus = FALSE OR LastScanned < DATE_SUB(NOW(), Interval 31 DAY)""" % self.scantable
 
-db = DataBase()
-db.populate("AppStore.GooglePlay_Master_DB")
+        print query
+
+        cursor = self.conn.cursor()
+        cursor.execute(query)
+
+        results = cursor.fetchall()
+
+        urls = [(row[0], 0) for row in results]
+
+        return urls
+
+    def updateStatus(self, urls):
+        query = ""
+        if self.scantable == 'GooglePlay':
+            query = """UPDATE GooglePlay SET ScanStatus = TRUE, LastScanned = NOW() WHERE URL = %s OR 1 = %s"""
+        elif self.scantable == 'AppStore':
+            query = """UPDATE AppStore SET ScanStatus = TRUE, LastScanned = NOW() WHERE URL = %s OR 1 = %s"""
+        
+        cursor = self.conn.cursor()
+        cursor.executemany(query, urls)
+
+        self.conn.commit()
+
+    def quit(self):
+        self.conn.close()
